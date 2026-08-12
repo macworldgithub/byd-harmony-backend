50b00d0857dd0b0bcf6be1972d944e0829fda7ce
syedjawwadkareem <syedjawwadkareem@gmail.com>
2026-08-12 12:06:01 +0500

    ..

--- dist/api-keys/dto/update-api-key.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateApiKeyDto } from './create-api-key.dto';
-declare const UpdateApiKeyDto_base: import("@nestjs/common").Type<Partial<Pick<CreateApiKeyDto, "name" | "isActive" | "role" | "locationId" | "department" | "scopes" | "webhookUrl" | "webhookSecret" | "retryStrategy" | "maxRetries">>>;
-export declare class UpdateApiKeyDto extends UpdateApiKeyDto_base {
-}
-export {};
